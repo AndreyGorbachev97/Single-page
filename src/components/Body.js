72dacc5d2b967c897ad1abbccbf97b4356f6d90c
Andrey_Gorbachev <andrew.gorba4ev2018@yandex.ru>
2019-01-20 23:22:12 +0300

    test commit IDE Vs code

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,15 +7,16 @@ import CarIcon from '@material-ui/icons/DirectionsCarOutlined';
 import RestaurantIcon from '@material-ui/icons/RestaurantOutlined';
 import Button from '@material-ui/core/Button';
 
+
 const styles = theme => ({
-    Body: { 
+    Body: {
         display: 'flex',
         justifyContent: 'center',
-        color: 'white',       
-        marginTop: '5%'      
+        color: 'white',
+        marginTop: '5%'
     },
     Title: {
-        fontSize: '50px', 
+        fontSize: '50px',
     },
     Table: {
         display: 'flex',
@@ -23,11 +24,11 @@ const styles = theme => ({
         fontSize: '18px',
         color: '#e0e0e0'
     },
-    Icon: { 
+    Icon: {
         width: 60,
         height: 60,
         color: '#e0e0e0'
-    }, 
+    },
     Button: {
         background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
         marginTop: '5%',
@@ -38,54 +39,54 @@ const styles = theme => ({
         height: 48,
         width: '90%',
         padding: '0 30px',
-      },
-  });
+    },
+});
 
-class Body extends Component{
+class Body extends Component {
 
-    
-    render(){
+
+    render() {
         console.log(this.props);
-        const {classes} = this.props
-        return(
+        const { classes } = this.props
+        return (
             <div className={classes.Body}>
-                <Grid container spacing={24} style={{width: '60%'}}>
+                <Grid container spacing={24} style={{ width: '60%' }}>
                     <Grid item xs={12}>
                         <div className={classes.Title}>
                             Лучший шоколад в каневском районе оптом!
                         </div>
                     </Grid>
                     <Grid item xs={4}>
-                        <CarIcon className={classes.Icon}/>
+                        <CarIcon className={classes.Icon} />
                         <span className={classes.Table}>
-                            Доставка всего 50 рублей 
+                            Доставка всего 50 рублей
                             в любое место станицы Каневской
                             и Стародеревянковской
-                        </span>                       
+                        </span>
                     </Grid>
-                    <Grid item xs={4} >                     
+                    <Grid item xs={4} >
                         <AssignmentIcon className={classes.Icon} />
-                        <span className={classes.Table}>                       
-                            Уникальное предложение, только у нас вы можете 
-                            покупать киллограммы замечательного шоколада
-                        </span>                          
-                    </Grid>     
-                    <Grid item xs={4}>
-                        <RestaurantIcon className={classes.Icon}/>
                         <span className={classes.Table}>
-                            На ваш выбор предоставляется несколько видов изиссканного 
+                            Уникальное предложение, только у нас вы можете
+                            покупать киллограммы замечательного шоколада
+                        </span>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <RestaurantIcon className={classes.Icon} />
+                        <span className={classes.Table}>
+                            На ваш выбор предоставляется несколько видов изиссканного
                             вкуса шоколада
-                        </span>            
-                    </Grid>   
-                    <Grid item xs={4}/>                                     
+                        </span>
+                    </Grid>
+                    <Grid item xs={4} />
                     <Grid item xs={4} >
                         <Button variant="outlined" className={classes.Button}>
                             Заказать
                         </Button>
-                    </Grid>   
-                    <Grid item xs={4}/>         
-                </Grid> 
-                   
+                    </Grid>
+                    <Grid item xs={4} />
+                </Grid>
+
             </div>
         )
     }
